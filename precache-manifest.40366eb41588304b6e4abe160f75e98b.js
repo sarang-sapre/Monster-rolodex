adf44f61548ad882df9ed1c722a485b13344a867
sarang-sapre <57029965+sarang-sapre@users.noreply.github.com>
2019-11-25 00:17:36 +0530

    Updates

---
+++ precache-manifest.40366eb41588304b6e4abe160f75e98b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84fb7451eb38325d7ce7aadf5a10606",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "abc00d83bd211470bb12",
+    "url": "/Monster-rolodex/static/css/main.7e4a40cd.chunk.css"
+  },
+  {
+    "revision": "8256d7d0304953484fc5",
+    "url": "/Monster-rolodex/static/js/2.f5605220.chunk.js"
+  },
+  {
+    "revision": "abc00d83bd211470bb12",
+    "url": "/Monster-rolodex/static/js/main.efceb2bc.chunk.js"
+  },
+  {
+    "revision": "3a4707fe9bdb24ede2c8",
+    "url": "/Monster-rolodex/static/js/runtime-main.e4ab96ef.js"
+  }
+]);
